04425c92308211ce515a8904572a4cec1ae197ec
Juliano <kurdpemgs@gmail.com>
2026-04-15 11:51:34 -0300

    Add inserido

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-const url = "https://jsonplaceholder.typicode.com/users";
-// const url = "http://localhost:3000/products";
+// const url = "https://jsonplaceholder.typicode.com/users";
+const url = "http://localhost:3000/users";
 
 function App() {
   const [users, setUsers] = useState([]);
+
+  const [name, setName] = useState([]);
+  const [username, setUsername] = useState([]);
 
   //93(1) - Resgatando dados
 
@@ -20,6 +23,25 @@ function App() {
 
     fetchData(users);
   }, []);
+
+  //94(2) - Add users
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      username,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+  };
 
   return (
     <>
@@ -38,6 +60,29 @@ function App() {
             </li>
           ))}
         </ul>
+        <div className="add-user">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input
+                type="text"
+                value={name}
+                name="Nome"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <label>
+              Usuário na rede:
+              <input
+                type="text"
+                value={username}
+                name="Usuário de rede"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <input type="submit" value="Criar" />
+          </form>
+        </div>
       </div>
     </>
   );
